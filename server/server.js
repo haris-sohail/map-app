8c48e5f0bd5be54824e6da51a2bc03dbb90c9806
Haris Sohail <harissohail.cs@gmail.com>
2024-06-24 10:24:01 +0500

    Init the projec

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("connected");
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("Server listening the port http://localhost/" + port);
+});
