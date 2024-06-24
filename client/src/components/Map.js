6ee60d86f08e40fed2caeabb2a6137a569b6ea95
Haris Sohail <harissohail.cs@gmail.com>
2024-06-24 11:38:00 +0500

    Add a temporary icon for testing

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { MapContainer, Marker, TileLayer } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
+import { Icon } from 'leaflet'
 
 function Map() {
+    const icon = new Icon({
+        iconUrl:'https://cdn-icons-png.flaticon.com/128/447/447031.png',
+        iconSize: [40, 40]
+    })
     return (
         <div>
             <MapContainer center={[33.6995, 73.0363]} zoom={13} className='h-screen'>
@@ -10,6 +15,8 @@ function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
+
+                <Marker position={[33.6995, 73.0363]} icon={icon}/>
             </MapContainer>
         </div>
     )
