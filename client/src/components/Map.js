da3da41a9fca1a52b24bab25ff9d74d6305c29fa
Haris Sohail <harissohail.cs@gmail.com>
2024-06-25 10:58:16 +0500

    Add region highlighting upon selection of a location

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
-import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMap, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
+import geojsonData from '../geojson/regions.json';
 
 function Map({ locations }) {
     const icon = new Icon({
@@ -17,12 +18,38 @@ function Map({ locations }) {
         return null;
     }
 
-    // Use the last location to center the map
-    const centerLocation = locations[locations.length - 1];
+    const centerLocation = locations.length > 0 ? locations[locations.length - 1] : [33.6844, 73.0479]; // Default to Islamabad if no locations
+
+    const pointInPolygon = (point, polygon) => {
+        let x = point[1], y = point[0];
+        let inside = false;
+        for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+            let xi = polygon[i][0], yi = polygon[i][1];
+            let xj = polygon[j][0], yj = polygon[j][1];
+
+            let intersect = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+            if (intersect) inside = !inside;
+        }
+        return inside;
+    };
+
+    const getRegionStyle = (feature) => {
+        const coordinates = feature.geometry.coordinates[0];
+        const selected = locations.some(location =>
+            pointInPolygon(location, coordinates)
+        );
+
+        return {
+            color: selected ? 'red' : 'blue',
+            weight: 2,
+            fillColor: selected ? 'green' : 'yellow',
+            fillOpacity: 0.2
+        };
+    };
 
     return (
         <div>
-            <MapContainer center={[centerLocation[0], centerLocation[1]]} zoom={8} className='h-screen'>
+            <MapContainer center={[centerLocation[0], centerLocation[1]]} zoom={16} className='h-screen'>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -31,6 +58,7 @@ function Map({ locations }) {
                 {locations.map((location, index) => (
                     <Marker key={index} position={[location[0], location[1]]} icon={icon} />
                 ))}
+                <GeoJSON data={geojsonData} style={getRegionStyle} />
             </MapContainer>
         </div>
     );
